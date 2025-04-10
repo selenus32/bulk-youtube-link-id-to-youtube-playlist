61282e3b78e0c54ab3328056d58f13bb5481e60c
selenus32 <126150306+selenus32@users.noreply.github.com>
2025-04-10 18:51:56 +1000

    new api version

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from ytmusicapi import YTMusic
-import os
+ytmusic = YTMusic("browser.json")
 
-USER_ID = os.getenv('USER_ID')
-
-playlist_name = 'INPUT NAME HERE'
+playlist_name = 'INPUT HERE'
 playlist_description = 'INPUT DESCRIPTION HERE'
 
-class YTvideoIDtoPlaylist:
-    def __init__(self):
-        self.api = YTMusic('headers_auth.json', f'{USER_ID}')
+playlistId = ytmusic.create_playlist(playlist_name, playlist_description)
 
-    def create_playlist(self, name, info, privacy="PRIVATE", tracks=None):
-        return self.api.create_playlist(name, info, privacy, video_ids=tracks)
-
-def main():
-    ytmusic = YTvideoIDtoPlaylist()
-
-    with open('video_ids.txt', 'r') as f:
+with open('video_ids.txt', 'r') as f:
         lines = f.readlines()
         videoIds = [line.strip() for line in lines]
-        
-    playlistId = ytmusic.create_playlist(playlist_name, playlist_description,'PRIVATE', videoIds)
-    print("Created playlist from IDs: \"" + playlist_name + "\"\n" +
-            "https://music.youtube.com/playlist?list=" + playlistId)
 
-if __name__ == "__main__":
-    main()
+ytmusic.add_playlist_items(playlistId, videoIds)
+print("Created playlist '" + playlist_name + "':" + "\"\n" + "https://music.youtube.com/playlist?list=" + playlistId)
